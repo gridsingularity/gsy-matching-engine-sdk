7c1e3292cf9dfd449037a81454d34ba4678791e6
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-05-05 12:58:22 +0300

    Added requirements

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+from myko_api_client import __version__
+
+target_branch = os.environ.get('BRANCH', 'master')
+
+
+try:
+    with open('requirements/base.txt') as req:
+        REQUIREMENTS = [r.partition('#')[0] for r in req if not r.startswith('-e')]
+        REQUIREMENTS.extend(
+            ['d3a-interface @ '
+             f'git+https://github.com/gridsingularity/d3a-interface.git@{ target_branch }'])
+
+except OSError:
+    # Shouldn't happen
+    REQUIREMENTS = []
+
+with open("README.md", "r") as readme:
+    README = readme.read()
+
+# *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+VERSION = __version__
+
+setup(
+    name="myko-api-client",
+    description="Myko API Client",
+    long_description=README,
+    author='GridSingularity',
+    author_email='d3a@gridsingularity.com',
+    url='https://github.com/gridsingularity/myko-api-client',
+    version=VERSION,
+    packages=find_packages(where=".", exclude=["tests"]),
+    package_dir={"myko_api_client": "myko_api_client"},
+    package_data={},
+    install_requires=REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+            'myko = myko_api_client.cli:main',
+        ]
+    },
+    zip_safe=False,
+)
