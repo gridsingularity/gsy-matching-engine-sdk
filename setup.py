72279856e68a2e6311c2f199342beceba7e0906d
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-09-10 14:31:19 +0200

    D3ASIM-3572: fix pylint warnings in setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+"""Setup module for the myco-api-client."""
+
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
+
 from myco_api_client import __version__
 
 TARGET_BRANCH = os.environ.get("TARGET_BRANCH", "master")
 
 
 try:
-    with open('requirements/base.txt') as req:
-        REQUIREMENTS = [r.partition('#')[0] for r in req if not r.startswith('-e')]
+    with open("requirements/base.txt", encoding="utf-8") as req:
+        REQUIREMENTS = [r.partition("#")[0] for r in req if not r.startswith("-e")]
         REQUIREMENTS.extend(
             ["d3a-interface @ "
              f"git+https://github.com/gridsingularity/d3a-interface.git@{TARGET_BRANCH}"])
@@ -16,7 +20,8 @@ except OSError:
     # Shouldn't happen
     REQUIREMENTS = []
 
-with open("README.md", "r") as readme:
+
+with open("README.md", "r", encoding="utf-8") as readme:
     README = readme.read()
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
@@ -26,17 +31,17 @@ setup(
     name="myco-api-client",
     description="Myco API Client",
     long_description=README,
-    author='GridSingularity',
-    author_email='d3a@gridsingularity.com',
-    url='https://github.com/gridsingularity/myco-api-client',
+    author="GridSingularity",
+    author_email="d3a@gridsingularity.com",
+    url="https://github.com/gridsingularity/myco-api-client",
     version=VERSION,
     packages=find_packages(where=".", exclude=["tests"]),
     package_dir={"myco_api_client": "myco_api_client"},
     package_data={},
     install_requires=REQUIREMENTS,
     entry_points={
-        'console_scripts': [
-            'myco = myco_api_client.cli:main',
+        "console_scripts": [
+            "myco = myco_api_client.cli:main",
         ]
     },
     zip_safe=False,
