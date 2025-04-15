f6effcb7d0f91660ef2041d6ca93bc807ef887e2
hannesdiedrich <hannes@gridsingularity.com>
2025-04-15 13:17:03 +0200

    GSYE-498: Fix tox and flake8

--- gsy_matching_engine_sdk/websocket_device.py
+++ gsy_matching_engine_sdk/websocket_device.py
@@ -4,11 +4,14 @@ from gsy_framework.client_connections.utils import log_market_progression
 
 
 class WebsocketMessageReceiver:
+    """WebsocketMessageReceiver"""
+
     def __init__(self, rest_client):
         self.client = rest_client
 
     def _handle_event_message(self, message):
-        """Available events: market, tick, finish, offers_bids_response, matched_recommendations_response.
+        """Available events: market, tick, finish, offers_bids_response,
+           matched_recommendations_response.
 
         Args:
             message: Received websocket message
@@ -22,6 +25,7 @@ class WebsocketMessageReceiver:
             getattr(self.client, f"_on_{event}")(message)
 
     def received_message(self, message):
+        """Handle received message."""
         try:
             self._handle_event_message(message)
             self.client.on_event_or_response(message)
